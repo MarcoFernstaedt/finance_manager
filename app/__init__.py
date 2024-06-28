5233d33d5c480f898bac01e2f9b6768c39d66584
Marco Fernstaedt <fernstaedtmarco@gmail.com>
2024-06-28 14:44:33 -0700

    wip: flask mail config

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from dotenv import load_dotenv
+from flask_mail import Mail
 import os
 
 load_dotenv()  # Load environment variables from .env file
